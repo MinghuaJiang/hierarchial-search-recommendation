9bbe20b54e2a708fcb641d5875d12790b1072a40
Minghua Jiang <61221630@qq.com>
2017-04-28 19:25:52 -0400

    initial check in

--- src/main/java/edu/virginia/cs/core/algorithm/TagGraphGenerator.java
+++ src/main/java/edu/virginia/cs/core/algorithm/TagGraphGenerator.java
@@ -1,7 +1,15 @@
 package edu.virginia.cs.core.algorithm;
 
+import edu.virginia.cs.core.model.TagVector;
+
+import java.util.List;
+
 /**
  * Created by cutehuazai on 4/28/17.
  */
 public class TagGraphGenerator {
+
+    public String generateTagGraph(List<TagVector> tagVectors){
+        return "";
+    }
 }
